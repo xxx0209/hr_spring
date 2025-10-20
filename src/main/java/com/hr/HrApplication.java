2e4074da8b43acb201fe5063a736807250120c52
ljy <makelotus0209@gmail.com>
2025-10-20 11:32:12 +0900

    first commit

---
+++ src/main/java/com/hr/HrApplication.java
@@ -0,0 +1,14 @@
+package com.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApplication.class, args);
+		System.out.println("스타트");
+	}
+
+}
