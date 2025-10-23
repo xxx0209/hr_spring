0323da538080ff677d7854859394d286580cf379
ljy <makelotus0209@gmail.com>
2025-10-23 18:34:32 +0900

    스프링 시큐리티 cros 수정

--- src/main/java/com/hr/config/SecurityConfig.java
+++ src/main/java/com/hr/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.hr.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -20,6 +21,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()    // 모든 요청 인증 없이 허용
                 )
