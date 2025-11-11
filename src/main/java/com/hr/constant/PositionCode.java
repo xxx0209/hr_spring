c820cc967ad03ced08f82b324090445344f9034c
ljy <makelotus0209@gmail.com>
2025-11-11 16:28:58 +0900

    위치 변경

--- src/main/java/com/hr/constant/PositionCode.java
+++ src/main/java/com/hr/constant/PositionCode.java
@@ -1,10 +1,12 @@
 package com.hr.constant;
 
 public enum PositionCode {
-    INTERN("인턴"),
-    STAFF("직원"),
-    ASSISTANT("대리"),
-    CEO("사장");
+//    INTERN("인턴"),
+//    STAFF("직원"),
+//    ASSISTANT("대리"),
+//    CEO("사장");
+
+    CEO("대표이사"), VP("부사장"), SED("전무"), ED("상무"), DIR("이사"), GM("부장"), DGM("차장"), MGR("과장"), AM("대리"), SS("주임"), STF("사원"), IN("인턴");
 
     private final String label;
 
