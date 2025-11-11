540946e7d1a4e28458c61d57c74c3daec5faa00d
ljy <makelotus0209@gmail.com>
2025-11-11 12:48:05 +0900

    스케줄 수정

--- src/main/java/com/hr/repository/ScheduleRepository.java
+++ src/main/java/com/hr/repository/ScheduleRepository.java
@@ -3,10 +3,14 @@ package com.hr.repository;
 import com.hr.entity.Schedule;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     // ✅ member, category를 함께 즉시 로딩
     @EntityGraph(attributePaths = {"member", "category"})
     List<Schedule> findByMemberId(String memberId);
+
+    List<Schedule> findByMemberIdAndStartBetween(String memberId, LocalDateTime start, LocalDateTime end);
 }
